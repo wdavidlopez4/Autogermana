666b318bb840c0753db982c3c2cc63b83b51d82e
william <wdavidlopez4@gmail.com>
2021-10-25 18:38:38 -0500

    creacion de puertos

---
+++ src/Autogermana.Domain/Ports/IAutoMapping.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autogermana.Domain.Ports
+{
+    public interface IAutoMapping
+    {
+        public TDestination Map<TSource, TDestination>(TSource source);
+    }
+}
---
+++ src/Autogermana.Domain/Ports/IRepository.cs
@@ -0,0 +1,51 @@
+﻿using Autogermana.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Autogermana.Domain.Ports
+{
+    public interface IRepository
+    {
+        /// <summary>
+        /// guarda objeto en la db
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task<T> Save<T>(T obj, CancellationToken cancellationToken) where T : Entity;
+
+        /// <summary>
+        /// verifica si existe el obj
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public bool Exists<T>(Expression<Func<T, bool>> expression) where T : Entity;
+
+        /// <summary>
+        /// retorna un objeto, el primero en cumplir el predicado
+        /// debe pararse datos unicos como el id, ya que, solo devolvera el primer objeto
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task<T> Get<T>(Expression<Func<T, bool>> expression,
+            CancellationToken cancellationToken) where T : Entity;
+
+        /// <summary>
+        /// modifica un objeto que tenga el mismo identificador
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task<T> Update<T>(T obj, CancellationToken cancellationToken) where T : Entity;
+    }
+}
